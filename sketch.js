f4a396cb748208e7064a74decccd659e5a96cbe0
TaynaraLima <00001113756421sp@al.educacao.sp.gov.br>
2024-05-27 08:13:37 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,8 @@
+ let cor;
+ let circuloX;// horizontal
+ let circuloY;// vertical
+
+function setup () {
+  createCanvas (400.400); // width x height
+  background (100.0.0));
+   cor=color(random(0,255), random
